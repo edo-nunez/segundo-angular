0b69b570738e10fee708bfdfbb1ec313592839bd
edo-nunez <ed.nunez@duocuc.cl>
2022-08-22 20:53:13 -0400

    Primera tarea

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,18 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'segundo-angular';
+  resultado:number=0;
+
+  sumar(numero1:number,numero2:number):number{
+    return numero1 + numero2;
+  }
+
+  restar(numero1:number,numero2:number):number{
+    return numero1 - numero2;
+  }
+
+  sumarDos(): void {
+    this.resultado = this.resultado + 2;
+  }
+
 }
